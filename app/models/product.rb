46376a33f139e2eaedfea99991e79d74d74478fe
alex <arodriguez202020@gmail.com>
2021-09-25 16:38:31 -0500

    add associations and validations to models, add calculation methods to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,32 @@
 class Product < ApplicationRecord
+  validates :name, uniqueness: true
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  validates :itemcount, presence: true
+
+
+
+  belongs_to :supplier
+
+  has_many :product_categories
+  has_many :categories, through: :product_categories
+
+  has_many :cartedProducts
+  has_many :orders, through: :cartedProducts
+
+  def is_discounted?
+    price.to_i <= 10 
+  end
+
+  def tax
+    tax = price.to_i * 0.09
+    return tax
+  end
+
+  def total
+    sum = price.to_i + tax
+    return sum
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+
+  has_many :orders
+  has_many :cartedProducts
+  has_many :products, through: :carted_products
 end
