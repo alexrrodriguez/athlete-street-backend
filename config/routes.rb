71a19f3f957ba9e927b79619c2d034af941cb556
alex <arodriguez202020@gmail.com>
2021-10-09 12:33:51 -0500

    update routes for female products

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   get "/popular" => "products#popular"
 
-  get "/popular" => "products#male"
+  get "/male" => "products#male"
+
+  get "/female" => "products#female"
 
   get "/products/:id" => "products#show"
 
